702c802de9ca0c37da938fbacec0cad3f24dab08
zl <467171232@qq.com>
2018-02-14 21:26:47 +0800

    搭建框架

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/main_amd64_linux main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o ./bin/main_i386_linux main.go 
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./bin/main_amd64_windows.exe main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o ./bin/main_i386_windows.exe main.go
+
